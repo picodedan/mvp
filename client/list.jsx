66d757051ba6b5ab68375213948c0bc81a756ad7
ddarrach314 <ddarrach314@gmail.com>
2017-08-05 10:20:35 -0700

    added trip and list files for functional components of page.

---
+++ client/list.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+// list element will take input from trip element
+
+// will render each item of the array/object passed to it as a clickable list 
+//each list item when clicked will toggle 'strikethrough' 
+
+//if all items are 'struckthrough' show and alert or additional colorful element that indicates/celebrates completion. 
+
